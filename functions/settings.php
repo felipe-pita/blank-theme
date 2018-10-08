65a470be6342a40ee38c7d0a303d56d6c1bd1d5d
Pita <felipedefarias@live.com>
2018-10-08 15:06:20 -0300

    Separa o support, dos menus

--- functions/settings.php
+++ functions/settings.php
@@ -6,8 +6,13 @@
 function set_theme_support() {
     add_theme_support('title-tag');
     add_theme_support('html5');
+}
+add_action('after_setup_theme', 'set_theme_support');
 
-    // Menus
+/**
+ * Registra os menus
+ */
+function register_menus() {
     register_nav_menus(
 		array('filter-sidebar' => 'Filtros na Sidebar')
 	);
